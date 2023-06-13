e266ed957b47bdf157023e96c3d5f5b20713e8b7
RominaValoriani <rominavaloriani@gmail.com>
2023-06-13 12:22:14 -0300

    agrego link prod y desarrollo

--- src/main/java/com/portfolio/rgv/Controller/CEducacion.java
+++ src/main/java/com/portfolio/rgv/Controller/CEducacion.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontend-ap-rv.web.app")
+@CrossOrigin(origins = {"https://frontend-ap-rv.web.app","http://localhost:4200"})
 
 public class CEducacion {
     @Autowired
